9e8654d32fc7d89390740210aa25c2ebafd9873a
apreynolds1989 <apreynolds1989@gmail.com>
2022-02-22 20:06:49 -0330

    Add descriptions
    Add explanations.

--- main.js
+++ main.js
@@ -3,13 +3,15 @@ recommended_players = [
     ["Ruben", "Chevez"] //row 2
 ]
 
-
+//Reference the table
 var tbodyRef = document.getElementById('recommended-players');
 
 
+//Iterate through the rows
 recommended_players.forEach(rowContent => {
     var tempRow = document.createElement('tr'); //temporary row
 
+    //Iterate trhough the cells
     rowContent.forEach(cellContent => {
         var tempCell = document.createElement('td'); 
         tempCell.innerHTML = cellContent

71aa1d2b221258bf66ea1937639f83b874cf8806
apreynolds1989 <apreynolds1989@gmail.com>
2022-03-04 16:40:53 -0330

    Handling Different Screen Sizes
    Updated CSS and Javascript

--- main.js
+++ main.js
@@ -10,21 +10,44 @@ weeklyGames = [
     ["Player 6", 2, 0, 51, 24, 25, 49, 20, 16, 9]
 ];
 
-//Reference the table
-const weeklyGamesTableBodyRef = document.getElementById('weeklyGames');
+function populateWeeklyGamesTable(weeklyGames, tableId, isMobile){
+    //Reference the table
+    const weeklyGamesTableBodyRef = document.getElementById(tableId);
 
-//Iterate through the rows
-weeklyGames.forEach(weeklyGamesRowContent => {
-   const weeklyGamesTempRow = document.createElement('tr'); //temporary row
-   //Iterate trhough the cells
-   weeklyGamesRowContent.forEach(weeklyGamesCellContent => {
-       const weeklyGamesTempCell = document.createElement('td'); 
-       weeklyGamesTempCell.innerHTML = weeklyGamesCellContent;
-       weeklyGamesTempRow.append(weeklyGamesTempCell);
-   });
-   weeklyGamesTableBodyRef.append(weeklyGamesTempRow);
-});
+    //Iterate through the rows
+    for (let row_index = 0; row_index < weeklyGames.length; row_index++) {
+        const weeklyGamesRowContent = weeklyGames[row_index];
+        const weeklyGamesTempRow = document.createElement('tr'); //temporary row
+        
+        for (let column_index = 0; column_index < weeklyGamesRowContent.length; column_index++) {
+            const weeklyGamesCellContent = weeklyGamesRowContent[column_index];
 
+            if(isMobile == true && column_index > 6)
+            {
+                continue;
+            }
+            const weeklyGamesTempCell = document.createElement('td'); 
+            weeklyGamesTempCell.innerHTML = weeklyGamesCellContent;
+            weeklyGamesTempRow.append(weeklyGamesTempCell);
+            
+        }
+        weeklyGamesTableBodyRef.append(weeklyGamesTempRow);
+    }
+    
+    // weeklyGames.forEach(weeklyGamesRowContent => {
+    //     const weeklyGamesTempRow = document.createElement('tr'); //temporary row
+    //     //Iterate trhough the cells
+    //     weeklyGamesRowContent.forEach(weeklyGamesCellContent => {
+    //         const weeklyGamesTempCell = document.createElement('td'); 
+    //         weeklyGamesTempCell.innerHTML = weeklyGamesCellContent;
+    //         weeklyGamesTempRow.append(weeklyGamesTempCell);
+    //     });
+    //     weeklyGamesTableBodyRef.append(weeklyGamesTempRow);
+    // });
+}
+
+populateWeeklyGamesTable(weeklyGames, 'weeklyGames', false)
+populateWeeklyGamesTable(weeklyGames, 'weeklyGamesMobile', true)
 
 // Array for listing players in table
 recommendedPlayers = [
@@ -41,12 +64,12 @@ const recommendedPlayersTableBodyRef = document.getElementById('recommendedPlaye
 
 //Iterate through the rows
 recommendedPlayers.forEach(recommendedPlayersRowContent => {
-   const recommendedPlayersTempRow = document.createElement('tr'); //temporary row
-   //Iterate trhough the cells
-   recommendedPlayersRowContent.forEach(recommendedPlayersCellContent => {
-       const recommendedPlayersTempCell = document.createElement('td'); 
-       recommendedPlayersTempCell.innerHTML = recommendedPlayersCellContent;
-       recommendedPlayersTempRow.append(recommendedPlayersTempCell);
-   });
-   recommendedPlayersTableBodyRef.append(recommendedPlayersTempRow);
+const recommendedPlayersTempRow = document.createElement('tr'); //temporary row
+//Iterate trhough the cells
+recommendedPlayersRowContent.forEach(recommendedPlayersCellContent => {
+    const recommendedPlayersTempCell = document.createElement('td'); 
+    recommendedPlayersTempCell.innerHTML = recommendedPlayersCellContent;
+    recommendedPlayersTempRow.append(recommendedPlayersTempCell);
+});
+recommendedPlayersTableBodyRef.append(recommendedPlayersTempRow);
 });

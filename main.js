b844312d8487ad4a715a7aaa29505b7a8b2e527f
apreynolds1989 <apreynolds1989@gmail.com>
2022-04-12 18:32:56 -0230

    Started transition to Dynamic content from Undocumented NHL API; Generated Array of skater Name's and Id's; Generated Array of Goalie Names and Id's; Generated Array of Skater Stats
    Need to merge to main

--- main.js
+++ main.js
@@ -1,5 +1,72 @@
+let xhr = new XMLHttpRequest();
+let listOfTeams = [];
+let skatersArr = [];
+let goaliesArr = [];
+let num;
+
+xhr.open('GET', 'https://statsapi.web.nhl.com/api/v1/teams?expand=team.roster', true);
+
+xhr.onload = function() {
+    teamsObj = JSON.parse(this.responseText);
+    listOfTeams = teamsObj.teams;
+    console.log(teamsObj);
+    console.log(listOfTeams);
+    for (let i = 0; i < listOfTeams.length; i++) {
+        let teamRoster = listOfTeams[i].roster.roster;
+        for (let j = 0; j < teamRoster.length; j++) {
+            let goalieNameAndId = [];
+            let skaterNameAndId = [];
+            if (teamRoster[j].position.code === 'G') {
+                goalieNameAndId.push({ name: teamRoster[j].person.fullName, Id: teamRoster[j].person.id });
+                goaliesArr.push(goalieNameAndId);
+            } else {
+                skaterNameAndId.push({ name: teamRoster[j].person.fullName, Id: teamRoster[j].person.id });
+                skatersArr.push(skaterNameAndId);
+            };
+        }
+    }
+    getSkaterStats(skatersArr);
+}
+
+xhr.send();
+
+console.log(goaliesArr);
+console.log(skatersArr);
+
+
+//Getting an individual player's stats
+function getSkaterStats(Arr) {
+    let output = [];
+
+    for (let i = 0; i < Arr.length; i++) {
+        let xhrFunc = new XMLHttpRequest();
+        num = Arr[i][0].Id;
+
+        xhrFunc.open('GET', 'https://statsapi.web.nhl.com/api/v1/people/' + num + '/stats?stats=statsSingleSeason&season=20212022', true);
+
+        xhrFunc.onload = function() {
+            let playerStats = JSON.parse(this.responseText);
+            playerStats = playerStats.stats
+
+            //NEED TO DEAL WITH UNDEFINED 'stat' ERROR
+
+            let playerGoals = playerStats[0].splits[0].stat.goals;
+            let playerAssists = playerStats[0].splits[0].stat.assists;
+            let playerPoints = playerStats[0].splits[0].stat.points;
+
+            output.push(`${Arr[i][0].name} has scored ${playerGoals} goals and ${playerAssists} assists for ${playerPoints} points on the year!`);
+
+            //Print to HTML if Needed?
+            //document.querySelector('#player').insertAdjacentHTML('beforeend', output);
+        }
+
+        xhrFunc.send();
+    }
+    console.log(output);
+}
+
 //Create Array for weekly games
-weeklyGames = [
+let weeklyGames = [
     ["Player", "Weekly Games", "Off-Day Games", "Games Played", "Goals", "Assists", "Points", "Powerplay Points", "Hits", "Blocks"],
     ["Player 1", 4, 2, 55, 12, 32, 44, 13, 21, 7],
     ["Player 2", 3, 2, 53, 21, 44, 65, 11, 5, 5],
@@ -70,3 +137,38 @@ recommendedPlayers.forEach(recommendedPlayersRowContent => {
     });
     recommendedPlayersTableBodyRef.append(recommendedPlayersTempRow);
 });
+
+/* JQuery table to be added later */
+
+// $.get("https://statsapi.web.nhl.com/api/v1/teams", function(data) {
+//     console.log(
+//         data["teams"]
+//     );
+//     var team_list = data["teams"]
+//     var trHTML = '';
+//     for (let index = 0; index < team_list.length; index++) {
+//         const single_team = team_list[index];
+//         console.log(single_team);
+//         trHTML += '<tr><td>' + single_team["name"] + '</td> <td>' + single_team["abbreviation"] + '</td> </tr>';
+//     }
+//     $('#jquery-table').html(trHTML);
+
+// }); 
+
+/* 
+Ruben's Example 
+
+var settings = {
+    "url": "https://statsapi.web.nhl.com/api/v1/teams",
+    "method": "GET",
+    "timeout": 0,
+};
+$.ajax(settings).done(function(data) {
+    console.log("Teams: ", data.teams);
+    var team_list = data.teams.slice(0, 5)
+    console.log("Filling div: ");
+    // $('#teamsAPIexample').html(team_list[0].name);
+    team_list.forEach(team => {
+        $('#teamsAPIexample').append("<tr> <td>" + team.name + "</td><td><strong>" + team.abbreviation + "</strong> </td> </tr>")
+    });
+}); */

dd64923304ddf3baa5c3e2403fbdc4fcfe670ea1
apreynolds1989 <apreynolds1989@gmail.com>
2022-05-01 02:37:54 -0230

    Updated JSTables; skaters Table populates sorted by 'Points'; Goalies Table populates sorted by 'Wins'

--- main.js
+++ main.js
@@ -21,6 +21,49 @@ async function generateGoalieTable(arr) {
     generateGoaliesDataTables(goalieTableContent);
 };
 
+//Implement JSTables Library
+function generateSkatersDataTables(Arr) {
+    populateTable(Arr, "skatersTableData", false);
+    populateTable(Arr, "skatersTableDataMobile", true);
+    let skaterDataTable = new JSTable("#skatersTable", {
+        columns: [{
+            select: 7,
+            sortable: true,
+            sort: "desc",
+            searchable: true,
+        }, ]
+    });
+    let skaterDataTableMobile = new JSTable("#skatersTableMobile", {
+        columns: [{
+            select: 7,
+            sortable: true,
+            sort: "desc",
+            searchable: true,
+        }, ]
+    });
+}
+
+function generateGoaliesDataTables(Arr) {
+    populateTable(Arr, "goaliesTableData", false);
+    populateTable(Arr, "goaliesTableDataMobile", true);
+    let goaliesDataTable = new JSTable("#goaliesTable", {
+        columns: [{
+            select: 6,
+            sortable: true,
+            sort: "desc",
+            searchable: true,
+        }, ]
+    });
+    let goaliesDataTableMobile = new JSTable("#goaliesTableMobile", {
+        columns: [{
+            select: 6,
+            sortable: true,
+            sort: "desc",
+            searchable: true,
+        }, ]
+    });
+}
+
 //Function to call NHL API and return a list of all NHL teams
 async function getTeams() {
     let url = 'https://statsapi.web.nhl.com/api/v1/teams?expand=team.roster';
@@ -380,269 +423,17 @@ async function getGoalieStats(gamesArr) {
     return goalieStatsArr; */
 };
 
-/* function getSkaterStats(Arr, tableId, isMobile) {
-    for (let i = 0; i < Arr.length; i++) {
-        let num = Arr[i][0].Id;
-        let teamNum = Arr[i][0].teamId;
-        makeAjaxCall('GET', 'https://statsapi.web.nhl.com/api/v1/people/' + num + '/stats?stats=statsSingleSeason&season=20212022', true, (playerStats) => {
-            playerStats = playerStats.stats[0].splits;
-
-            if (playerStats.length > 0) {
-                let gamesPlayed = playerStats[0].stat.games;
-                let playerGoals = playerStats[0].stat.goals;
-                let playerAssists = playerStats[0].stat.assists;
-                let playerPoints = playerStats[0].stat.points;
-                let playerGameWinningGoals = playerStats[0].stat.gameWinningGoals;
-                let pointsPerGame = roundPrecision((playerPoints / gamesPlayed), 2);
-                let playerTOIperGame = playerStats[0].stat.timeOnIcePerGame;
-                let playerPPGoals = playerStats[0].stat.powerPlayGoals;
-                let playerPPP = playerStats[0].stat.powerPlayPoints;
-                let playerPPTOIperGame = playerStats[0].stat.powerPlayTimeOnIcePerGame;
-                let playerShortHandedGoals = playerStats[0].stat.shortHandedGoals;
-                let playerShortHandedPoints = playerStats[0].stat.shortHandedPoints;
-                let playerSHTOIperGame = playerStats[0].stat.shortHandedTimeOnIcePerGame;
-                let playerHits = playerStats[0].stat.hits;
-                let playerBlocks = playerStats[0].stat.blocked;
-                let playerShots = playerStats[0].stat.shots;
-                let playerShootingPct = playerStats[0].stat.shotPct;
-                let playerfaceoffPct = playerStats[0].stat.faceOffPct;
-                let playerPim = playerStats[0].stat.pim;
-
-                //Generate the array to be appended to the table
-                // weekly games and off day games are generated through function
-                let results = [
-                    Arr[i][0].name,
-                    Arr[i][0].team,
-                    gamesPlayed,
-                    generateWeeklyGamesTally(weeklyGames, teamNum),
-                    generateWeeklyOffDayGamesTally(weeklyGames, teamNum),
-                    playerGoals,
-                    playerAssists,
-                    playerPoints,
-                    playerGameWinningGoals,
-                    pointsPerGame,
-                    playerTOIperGame,
-                    playerPPGoals,
-                    playerPPP,
-                    playerPPTOIperGame,
-                    playerShortHandedGoals,
-                    playerShortHandedPoints,
-                    playerSHTOIperGame,
-                    playerHits,
-                    playerBlocks,
-                    playerShots,
-                    playerShootingPct,
-                    playerfaceoffPct,
-                    playerPim
-                ];
-                renderSingleRow(results, tableId, isMobile)
-            };
-        })
-    }
-}; */
-
-/* function getGoalieStats(Arr, tableId, isMobile) {
-    for (let i = 0; i < Arr.length; i++) {
-        let num = Arr[i][0].Id;
-        let teamNum = Arr[i][0].teamId;
-        makeAjaxCall('GET', 'https://statsapi.web.nhl.com/api/v1/people/' + num + '/stats?stats=statsSingleSeason&season=20212022', true, (goalieStats) => {
-            goalieStats = goalieStats.stats[0].splits;
-            if (goalieStats.length > 0) {
-                let gamesPlayed = goalieStats[0].stat.games;
-                let gamesStarted = goalieStats[0].stat.gamesStarted;
-                let goalieWins = goalieStats[0].stat.wins;
-                let goalieLosses = goalieStats[0].stat.losses;
-                let goalieShutouts = goalieStats[0].stat.shutouts;
-                let goalieShotsAgainst = goalieStats[0].stat.shotsAgainst
-                let goalieSaves = goalieStats[0].stat.saves;
-                let goalieSavePercentage = roundPrecision(goalieStats[0].stat.savePercentage, 3);
-                let goalieGoalsAgainst = goalieStats[0].stat.goalsAgainst;
-                let goalieGoalAgainstAverage = roundPrecision((goalieStats[0].stat.goalAgainstAverage), 3);
-
-                //Generate the array to be appended to the table
-                // weekly games and off day games are generated through function
-                let results = [
-                    Arr[i][0].name,
-                    Arr[i][0].team,
-                    generateWeeklyGamesTally(weeklyGames, teamNum),
-                    generateWeeklyOffDayGamesTally(weeklyGames, teamNum),
-                    gamesPlayed,
-                    gamesStarted,
-                    goalieWins,
-                    goalieLosses,
-                    goalieShutouts,
-                    goalieShotsAgainst,
-                    goalieSaves,
-                    goalieSavePercentage,
-                    goalieGoalsAgainst,
-                    goalieGoalAgainstAverage
-                ];
-
-                renderSingleRow(results, tableId, isMobile)
-            };
-        });
-    }
-}; */
-
-//Generate the games for a given Week from NHL API
-// For now these dates are static
-/* function generateWeeklyGames(currentWeekGames) {
-    let dates = currentWeekGames.dates;
-    console.log(dates);
-    //Loop through the dates given and push all games to an array to be
-    // used in generateWeeklyGamesTally()
-    for (i = 0; i < dates.length; i++) {
-        weeklyGames.push(dates[i].games);
-    };
-}; */
-
-//Take generated weekly games array, loop through games for each day
-// then loop through each player and determine if their team id (variable) is listed
-//  on either day, if it is, increment playerGamesTally
-//   playerGamesTally will be appended to the table for each player
-/* function generateWeeklyGamesTally(Arr, variable) {
-    let playerGamesTally = 0;
-    for (i = 0; i < Arr.length; i++) {
-        let games = Arr[i];
-        for (j = 0; j < games.length; j++) {
-            let awayTeam = games[j].teams.away.team.id;
-            let homeTeam = games[j].teams.home.team.id;
-            //console.log(`Looking for ${variable} - Away: ${awayTeam} Home: ${homeTeam}`);
-            if (awayTeam === variable || homeTeam === variable) {
-                playerGamesTally++;
-            };
-        };
-    };
-    return playerGamesTally;
-};
-
-function generateWeeklyOffDayGamesTally(Arr, variable) {
-    let playerOffDayGamesTally = 0;
-    for (i = 0; i < Arr.length; i++) {
-        let games = Arr[i];
-        if (Arr.length % 2) {
-            if (i === 1 || i === 3 || i === 5) {
-                for (j = 0; j < games.length; j++) {
-                    let awayTeam = games[j].teams.away.team.id;
-                    let homeTeam = games[j].teams.home.team.id;
-                    if (awayTeam === variable || homeTeam === variable) {
-                        playerOffDayGamesTally++;
-                    };
-                };
-            };
-        } else {
-            if (i === 0 || i === 2 || i === 4 || i === 6) {
-                for (j = 0; j < games.length; j++) {
-                    let awayTeam = games[j].teams.away.team.id;
-                    let homeTeam = games[j].teams.home.team.id;
-                    if (awayTeam === variable || homeTeam === variable) {
-                        playerOffDayGamesTally++;
-                    };
-                };
-            };
-        }
-    };
-    return playerOffDayGamesTally;
-}; */
+//Run toPercision() on a category and account for undefined values
+// Make undefined values = 0
+function roundPrecision(number, n_integers) {
+    return number != null ? number.toPrecision(n_integers) : 0;
+}
 
 //Function to empty the table, to be called in pagination buttons allowing new table to be rendered
 /* function emptyTable(tableId) {
     const skatersTableBodyRef = document.getElementById(tableId);
     skatersTableBodyRef.innerHTML = "";
 } */
-
-//Attempting to implement JSTables Library
-function generateSkatersDataTables(Arr) {
-    populateTable(Arr, "skatersTableData", false);
-    populateTable(Arr, "skatersTableDataMobile", true);
-    let skaterDataTable = new JSTable("#skatersTable");
-    let skaterDataTableMobile = new JSTable("#skatersTableMobile");
-}
-
-function generateGoaliesDataTables(Arr) {
-    populateTable(Arr, "goaliesTableData", false);
-    populateTable(Arr, "goaliesTableDataMobile", true);
-    let goaliesDataTable = new JSTable("#goaliesTable");
-    let goaliesDataTableMobile = new JSTable("#goaliesTableMobile");
-}
-
-/* // When 'Previous' or 'Next' button is clicked, decrease/increase pagination variables, 
-//    empty the table and render new table
-const previousBtn = document.getElementById('paginationPrevious');
-previousBtn.addEventListener("click", function() {
-    if (paginationStart === 0) {
-        return;
-    } else {
-        paginationStart -= 20;
-        paginationEnd -= 20;
-
-        emptyTable("skatersTableData")
-        emptyTable("skatersTableDataMobile")
-
-        paginatedSkatersArr = skatersArr.slice(paginationStart, paginationEnd);
-        getSkaterStats(paginatedSkatersArr, "skatersTableData", false);
-        getSkaterStats(paginatedSkatersArr, "skatersTableDataMobile", true);
-    };
-});
-
-
-const nextBtn = document.getElementById('paginationNext');
-nextBtn.addEventListener("click", function() {
-    if (paginationEnd >= skatersArr.length) {
-        return;
-    } else {
-        paginationStart += 20;
-        paginationEnd += 20;
-
-        emptyTable("skatersTableData")
-        emptyTable("skatersTableDataMobile")
-
-        paginatedSkatersArr = skatersArr.slice(paginationStart, paginationEnd);
-        getSkaterStats(paginatedSkatersArr, "skatersTableData", false);
-        getSkaterStats(paginatedSkatersArr, "skatersTableDataMobile", true);
-    };
-});
-
-const previousBtnGoalies = document.getElementById('paginationPreviousGoalies');
-previousBtnGoalies.addEventListener("click", function() {
-    if (paginationStartGoalie === 0) {
-        return;
-    } else {
-        paginationStartGoalie -= 20;
-        paginationEndGoalie -= 20;
-
-        emptyTable("goaliesTableData")
-        emptyTable("goaliesTableDataMobile")
-
-        paginatedGoaliesArr = goaliesArr.slice(paginationStartGoalie, paginationEndGoalie);
-        getGoalieStats(paginatedGoaliesArr, "goaliesTableData", false);
-        getGoalieStats(paginatedGoaliesArr, "goaliesTableDataMobile", true);
-    };
-});
-
-
-const nextBtnGoalies = document.getElementById('paginationNextGoalies');
-nextBtnGoalies.addEventListener("click", function() {
-    if (paginationEndGoalie >= goaliesArr.length) {
-        return;
-    } else {
-        paginationStartGoalie += 20;
-        paginationEndGoalie += 20;
-
-        emptyTable("goaliesTableData")
-        emptyTable("goaliesTableDataMobile")
-
-        paginatedGoaliesArr = goaliesArr.slice(paginationStartGoalie, paginationEndGoalie);
-        getGoalieStats(paginatedGoaliesArr, "goaliesTableData", false);
-        getGoalieStats(paginatedGoaliesArr, "goaliesTableDataMobile", true);
-    };
-}); */
-
-//Run toPercision() on a category and account for undefined values
-// Make undefined values = 0
-function roundPrecision(number, n_integers) {
-    return number != null ? number.toPrecision(n_integers) : 0;
-}
 
 /* //Function to render a single row and append it to the referenced table. 
 // Allows logic to remain async
@@ -675,7 +466,7 @@ function populateTable(Arr, tableId, isMobile) {
         for (let col_index = 0; col_index < tableRowContent.length; col_index++) {
             const tableCellContent = tableRowContent[col_index];
 
-            if (isMobile == true && col_index > 4) {
+            if (isMobile == true && col_index > 7) {
                 continue;
             };
             const tableTempCell = document.createElement('td'); //temporary cell
